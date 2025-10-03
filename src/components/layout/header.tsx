72b7a8b36a0a675f07075aafe47843263ea1f15c
Adem Hamizi <ademhamizi2005@gmail.com>
2025-10-03 21:52:00 +0000

    add some padding/space from left (_for element <Orbit>_)

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -25,7 +25,7 @@ export function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center">
         <Link href="/" className="mr-6 flex items-center space-x-2">
-          <Orbit className="h-6 w-6 text-accent ml-2" />
+          <Orbit className="h-6 w-6 text-accent ml-4" />
           <span className="hidden font-bold sm:inline-block font-headline">
             AI Exoplanet Explorer
           </span>
